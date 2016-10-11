f02e28a85cb7ec89d76f04dd49c82fb89390c805
David Boddie <david@boddie.org.uk>
2016-10-12 00:51:43 +0200

    Included additional directories in the hosted environment.

--- tools/make-install-root.sh
+++ tools/make-install-root.sh
@@ -11,6 +11,7 @@ fi
 echo "Creating $2 from $1"
 
 mkdir -p $2
+mkdir -p $2/mail
 mkdir -p $2/n/cd
 mkdir -p $2/n/client/chan
 mkdir -p $2/n/client/dev
@@ -28,13 +29,17 @@ mkdir -p $2/n/remote
 mkdir -p $2/tmp
 mkdir -p $2/usr/$USER
 
+cp -r $1/acme $2/
 cp -r $1/appl $2/
 cp -r $1/dis $2/
+cp -r $1/doc $2/
 cp -r $1/fonts $2/
 cp -r $1/icons $2/
 cp -r $1/keydb $2/
 cp -r $1/lib $2/
+cp -r $1/locale $2/
 cp -r $1/man $2/
+cp -r $1/module $2/
 cp -r $1/services $2/
 cp -r $1/usr $2/
 cp -r $1/usr/inferno/lib $2/usr/$USER/
